7ab0200e27246b66be0e0172ddfef9c62e0edd89
Bagirych <4workonly1988@gmail.com>
2022-09-07 22:37:54 +0300

    1

--- src/main/java/repository/RepositoryOffer.java
+++ src/main/java/repository/RepositoryOffer.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class RepositoryOffer {
+package repository;
+
+import domain.Offer;
+
+public class RepositoryOffer {
+    private Offer[] tickets = new Offer[0];
+
+    public void add(Offer offer) {
+        Offer[] tmp = new Offer[tickets.length + 1];
+        System.arraycopy(tickets, 0, tmp, 0, tickets.length);
+        tmp[tmp.length - 1] = offer;
+        tickets = tmp;
+    }
+
+    public Offer[] getAll() {
+        return tickets;
+    }
+
 }
